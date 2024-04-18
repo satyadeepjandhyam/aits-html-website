f40c1eef4928847c91160d474a9a58233879cf8c
Lonka-Pardhu <lonkapardhu99@gmail.com>
2024-04-18 17:35:34 +0530

    fixes in footer >> blog hidden >> and other fixes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,32 @@
-// Function to make API call and update HTML
+var parser = new DOMParser();
+
 function fetchData() {
   fetch("https://admin.analogueitsolutions.satyasadhna.com/allblogs")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-      //   const message = data.message;
-      //   document.getElementById("data").innerHTML = `<p>${message}</p>`;
+      console.log("allBlogs Data >>>>>>> ", data);
+      const allBlogs = data.allBlogs;
+      // const recentBlogs = data.recentBlogs;
+      const htmlDoc = parser.parseFromString(allBlogs[1].blogData, "text/html");
+      const title = allBlogs[0].title;
+      let blogContent = htmlDoc.body.innerHTML;
+
+      let imageTag = "";
+      const imageTagRegex = /<img[^>]+src="([^">]+)"/;
+      const match = blogContent.match(imageTagRegex);
+      if (match) {
+        imageTag = match[0];
+      }
+      console.log("Title:", title);
+      console.log("Blog Content:", blogContent);
+      console.log("Image Tag:", imageTag);
+
+      const injectTestDiv = document.querySelector(".inject-test");
+      if (injectTestDiv) {
+        injectTestDiv.innerHTML = blogContent;
+      } else {
+        console.error("Div with class 'inject-test' not found.");
+      }
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
